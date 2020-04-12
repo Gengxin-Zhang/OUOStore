ae37b2e11929fbecff83040a801efc8664579627
zack <cai.guanyu@qq.com>
2020-04-12 23:43:41 +0800

    实现资源分类crud接口

--- backend/ouostore-admin/src/main/java/org/csu/ouostore/admin/controller/UmsResourceCategoryController.java
+++ backend/ouostore-admin/src/main/java/org/csu/ouostore/admin/controller/UmsResourceCategoryController.java
@@ -1,16 +1,17 @@
 package org.csu.ouostore.admin.controller;
 
+import cn.hutool.core.bean.BeanUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.csu.ouostore.common.api.CommonResult;
+import org.csu.ouostore.model.entity.UmsResourceCategory;
 import org.csu.ouostore.model.query.UmsResourceCategoryCreateParam;
 import org.csu.ouostore.service.UmsResourceCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * 后台资源分类管理
@@ -28,6 +29,29 @@ public class UmsResourceCategoryController {
     public CommonResult create(@RequestBody @Validated UmsResourceCategoryCreateParam categoryCreateParam) {
         boolean success =  resourceCategoryService.create(categoryCreateParam);
         return success ? CommonResult.OK("创建成功") : CommonResult.failed("创建失败,未知错误");
+    }
+
+    @ApiOperation("查询所有后台资源分类")
+    @GetMapping("")
+    public CommonResult<List<UmsResourceCategory>> queryAll() {
+        return CommonResult.OK(resourceCategoryService.list());
+    }
+
+    @ApiOperation("更新后台资源分类")
+    @PutMapping("/{id}")
+    public CommonResult update(@PathVariable Long id, @RequestBody @Validated UmsResourceCategoryCreateParam resourceCategoryCreateParam) {
+        UmsResourceCategory resourceCategory = new UmsResourceCategory();
+        BeanUtil.copyProperties(resourceCategoryCreateParam, resourceCategory);
+        resourceCategory.setId(id);
+        boolean success = resourceCategoryService.updateById(resourceCategory);
+        return success ? CommonResult.OK("更新成功") : CommonResult.failed("id不存在");
+    }
+
+    @ApiOperation("根据ID删除后台资源")
+    @DeleteMapping("/{id}")
+    public CommonResult delete(@PathVariable Long id) {
+        resourceCategoryService.removeById(id);
+        return CommonResult.OK("删除成功");
     }
 
 }
