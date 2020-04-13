c0fb9e7eff9a7ca57995d7d07f51988db056e246
zack <cai.guanyu@qq.com>
2020-04-13 15:25:20 +0800

    新增数据库结构.sql文件

---
+++ db/ouostore_structure.sql
@@ -0,0 +1,409 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : zack
+ Source Server Type    : MySQL
+ Source Server Version : 50729
+ Source Host           : localhost:3306
+ Source Schema         : ouostore
+
+ Target Server Type    : MySQL
+ Target Server Version : 50729
+ File Encoding         : 65001
+
+ Date: 13/04/2020 15:23:07
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for oms_cart_item
+-- ----------------------------
+DROP TABLE IF EXISTS `oms_cart_item`;
+CREATE TABLE `oms_cart_item` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `product_id` bigint(20) DEFAULT NULL,
+  `product_sku_id` bigint(20) DEFAULT NULL,
+  `member_id` bigint(20) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL COMMENT '购买数量',
+  `price` decimal(10,2) DEFAULT NULL COMMENT '添加到购物车的价格',
+  `product_pic` varchar(1000) DEFAULT NULL COMMENT '商品主图',
+  `product_name` varchar(500) DEFAULT NULL COMMENT '商品名称',
+  `product_sku_code` varchar(200) DEFAULT NULL COMMENT '商品sku条码',
+  `create_date` datetime DEFAULT NULL COMMENT '创建时间',
+  `modify_date` datetime DEFAULT NULL COMMENT '修改时间',
+  `delete_status` int(1) DEFAULT '0' COMMENT '是否删除',
+  `product_category_id` bigint(20) DEFAULT NULL COMMENT '商品分类',
+  `product_sn` varchar(200) DEFAULT NULL COMMENT '货号',
+  `product_attr` varchar(500) DEFAULT NULL COMMENT '商品销售属性:[{"key":"颜色","value":"颜色"},{"key":"容量","value":"4G"}]',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='购物车表';
+
+-- ----------------------------
+-- Table structure for oms_company_address
+-- ----------------------------
+DROP TABLE IF EXISTS `oms_company_address`;
+CREATE TABLE `oms_company_address` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `address_name` varchar(200) DEFAULT NULL COMMENT '地址名称',
+  `send_status` int(1) DEFAULT NULL COMMENT '默认发货地址：0->否；1->是',
+  `receive_status` int(1) DEFAULT NULL COMMENT '是否默认收货地址：0->否；1->是',
+  `name` varchar(64) DEFAULT NULL COMMENT '收发货人姓名',
+  `phone` varchar(64) DEFAULT NULL COMMENT '收货人电话',
+  `province` varchar(64) DEFAULT NULL COMMENT '省/直辖市',
+  `city` varchar(64) DEFAULT NULL COMMENT '市',
+  `region` varchar(64) DEFAULT NULL COMMENT '区',
+  `detail_address` varchar(200) DEFAULT NULL COMMENT '详细地址',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='公司发货/收货(接收退货)地址表';
+
+-- ----------------------------
+-- Table structure for oms_order
+-- ----------------------------
+DROP TABLE IF EXISTS `oms_order`;
+CREATE TABLE `oms_order` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '订单id',
+  `member_id` bigint(20) NOT NULL COMMENT '会员id',
+  `order_sn` varchar(64) DEFAULT NULL COMMENT '订单编号',
+  `create_time` datetime DEFAULT NULL COMMENT '提交时间',
+  `member_username` varchar(64) DEFAULT NULL COMMENT '用户帐号',
+  `total_price` decimal(10,2) DEFAULT NULL COMMENT '订单总金额',
+  `pay_type` int(1) DEFAULT NULL COMMENT '支付方式：0->未支付；1->支付宝；2->微信',
+  `status` int(1) DEFAULT NULL COMMENT '订单状态：0->待付款；1->待发货；2->已发货；3->已完成；4->已关闭；5->无效订单',
+  `delivery_company` varchar(64) DEFAULT NULL COMMENT '物流公司(顺丰快递/申通快递/...)',
+  `delivery_sn` varchar(64) DEFAULT NULL COMMENT '物流单号',
+  `auto_confirm_day` int(11) DEFAULT NULL COMMENT '自动确认收货时间（天）',
+  `receiver_name` varchar(100) NOT NULL COMMENT '收货人姓名',
+  `receiver_phone` varchar(32) NOT NULL COMMENT '收货人电话',
+  `receiver_post_code` varchar(32) DEFAULT NULL COMMENT '收货人邮编',
+  `receiver_province` varchar(32) DEFAULT NULL COMMENT '省份/直辖市',
+  `receiver_city` varchar(32) DEFAULT NULL COMMENT '城市',
+  `receiver_region` varchar(32) DEFAULT NULL COMMENT '区',
+  `receiver_detail_address` varchar(200) DEFAULT NULL COMMENT '详细地址',
+  `confirm_status` int(1) DEFAULT NULL COMMENT '确认收货状态：0->未确认；1->已确认',
+  `delete_status` int(1) NOT NULL DEFAULT '0' COMMENT '删除状态：0->未删除；1->已删除',
+  `payment_time` datetime DEFAULT NULL COMMENT '支付时间',
+  `delivery_time` datetime DEFAULT NULL COMMENT '发货时间',
+  `receive_time` datetime DEFAULT NULL COMMENT '确认收货时间',
+  `modify_time` datetime DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='订单表';
+
+-- ----------------------------
+-- Table structure for oms_order_item
+-- ----------------------------
+DROP TABLE IF EXISTS `oms_order_item`;
+CREATE TABLE `oms_order_item` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `order_id` bigint(20) DEFAULT NULL COMMENT '订单id',
+  `order_sn` varchar(64) DEFAULT NULL COMMENT '订单编号',
+  `product_id` bigint(20) DEFAULT NULL COMMENT '商品id',
+  `product_pic` varchar(500) DEFAULT NULL COMMENT '商品图片',
+  `product_name` varchar(200) DEFAULT NULL COMMENT '商品名称',
+  `product_price` decimal(10,2) DEFAULT NULL COMMENT '销售价格',
+  `product_quantity` int(11) DEFAULT NULL COMMENT '购买数量',
+  `product_sku_id` bigint(20) DEFAULT NULL COMMENT '商品sku id',
+  `product_sku_code` varchar(50) DEFAULT NULL COMMENT '商品sku条码',
+  `product_category_id` bigint(20) DEFAULT NULL COMMENT '商品分类id',
+  `product_attr` varchar(500) DEFAULT NULL COMMENT '商品销售属性:[{"key":"颜色","value":"颜色"},{"key":"容量","value":"4G"}] 购买时选择的选项',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='订单中所包含的商品';
+
+-- ----------------------------
+-- Table structure for oms_order_operate_log
+-- ----------------------------
+DROP TABLE IF EXISTS `oms_order_operate_log`;
+CREATE TABLE `oms_order_operate_log` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `order_id` bigint(20) DEFAULT NULL COMMENT '订单id',
+  `operate_man` varchar(100) DEFAULT NULL COMMENT '操作人：用户；系统；后台管理员',
+  `create_time` datetime DEFAULT NULL COMMENT '操作时间',
+  `order_status` int(1) DEFAULT NULL COMMENT '订单状态：0->待付款；1->待发货；2->已发货；3->已完成；4->已关闭；5->无效订单',
+  `note` varchar(500) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='订单操作历史记录';
+
+-- ----------------------------
+-- Table structure for oms_order_setting
+-- ----------------------------
+DROP TABLE IF EXISTS `oms_order_setting`;
+CREATE TABLE `oms_order_setting` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `unpaid_overtime` int(11) DEFAULT NULL COMMENT '未支付订单超时时间(分)',
+  `confirm_overtime` int(11) DEFAULT NULL COMMENT '发货后自动确认收货时间（天）',
+  `finish_overtime` int(11) DEFAULT NULL COMMENT '收货后自动完成交易时间，不能申请售后（天）',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='订单设置表';
+
+-- ----------------------------
+-- Table structure for pms_product
+-- ----------------------------
+DROP TABLE IF EXISTS `pms_product`;
+CREATE TABLE `pms_product` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `product_category_id` bigint(20) DEFAULT NULL,
+  `product_attribute_category_id` bigint(20) DEFAULT NULL,
+  `name` varchar(64) NOT NULL,
+  `pic` varchar(255) DEFAULT NULL COMMENT '展示图',
+  `product_sn` varchar(64) NOT NULL COMMENT '货号',
+  `delete_status` int(1) DEFAULT NULL COMMENT '删除状态：0->未删除；1->已删除',
+  `publish_status` int(1) DEFAULT NULL COMMENT '上架状态：0->下架；1->上架',
+  `price` decimal(10,2) DEFAULT NULL,
+  `description` text COMMENT '商品描述',
+  `original_price` decimal(10,2) DEFAULT NULL COMMENT '市场价',
+  `stock` int(11) DEFAULT NULL COMMENT 'product下所有sku库存之和',
+  `weight` decimal(10,2) DEFAULT NULL COMMENT '商品重量，默认为克',
+  `keywords` varchar(255) DEFAULT NULL,
+  `album_pics` varchar(255) DEFAULT NULL COMMENT '画册图片，连产品图片限制为5张，以逗号分割',
+  `product_category_name` varchar(255) DEFAULT NULL COMMENT '商品分类名称',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='商品信息';
+
+-- ----------------------------
+-- Table structure for pms_product_attribute
+-- ----------------------------
+DROP TABLE IF EXISTS `pms_product_attribute`;
+CREATE TABLE `pms_product_attribute` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `product_attribute_category_id` bigint(20) DEFAULT NULL COMMENT '对应表product_attribute_category',
+  `name` varchar(64) DEFAULT NULL COMMENT '规格/参数名',
+  `select_type` int(1) DEFAULT NULL COMMENT '属性选择类型：0->唯一；1->单选；2->多选',
+  `input_type` int(1) DEFAULT NULL COMMENT '属性录入方式：0->手工录入；1->从列表中选取',
+  `input_list` varchar(255) DEFAULT NULL COMMENT '可选值列表，以逗号隔开',
+  `sort` int(11) DEFAULT NULL COMMENT '排序字段：最高的可以单独上传图片',
+  `hand_add_status` int(1) DEFAULT NULL COMMENT '是否支持手动新增；0->不支持；1->支持',
+  `type` int(1) DEFAULT NULL COMMENT '属性的类型；0->属性(规格)；1->参数',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='商品属性(规格)参数表,一条记录对应一个规格/参数';
+
+-- ----------------------------
+-- Table structure for pms_product_attribute_category
+-- ----------------------------
+DROP TABLE IF EXISTS `pms_product_attribute_category`;
+CREATE TABLE `pms_product_attribute_category` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT NULL,
+  `attribute_count` int(11) DEFAULT '0' COMMENT '规格(购买时选项如 黑/白, 1岁/2岁)数量',
+  `param_count` int(11) DEFAULT '0' COMMENT '参数(通用参数)数量',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='产品规格及属性分类表';
+
+-- ----------------------------
+-- Table structure for pms_product_attribute_value
+-- ----------------------------
+DROP TABLE IF EXISTS `pms_product_attribute_value`;
+CREATE TABLE `pms_product_attribute_value` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `product_id` bigint(20) DEFAULT NULL COMMENT '商品id',
+  `product_attribute_id` bigint(20) DEFAULT NULL COMMENT '商品属性/参数id',
+  `value` varchar(64) DEFAULT NULL COMMENT '手动添加规格或参数的值，参数单值，规格有多个时以逗号隔开',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='存储产品参数信息的表';
+
+-- ----------------------------
+-- Table structure for pms_product_category
+-- ----------------------------
+DROP TABLE IF EXISTS `pms_product_category`;
+CREATE TABLE `pms_product_category` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT NULL,
+  `product_count` int(11) DEFAULT NULL COMMENT '分类下product数量',
+  `show_status` int(1) DEFAULT NULL COMMENT '显示状态：0->不显示；1->显示',
+  `icon` varchar(255) DEFAULT NULL COMMENT '图标',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='宠物一级分类,猫/狗/兔子...';
+
+-- ----------------------------
+-- Table structure for pms_sku_stock
+-- ----------------------------
+DROP TABLE IF EXISTS `pms_sku_stock`;
+CREATE TABLE `pms_sku_stock` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `product_id` bigint(20) DEFAULT NULL,
+  `sku_code` varchar(64) NOT NULL COMMENT 'sku编码',
+  `price` decimal(10,2) DEFAULT NULL,
+  `stock` int(11) DEFAULT '0' COMMENT '库存',
+  `pic` varchar(255) DEFAULT NULL COMMENT '展示图片',
+  `sale` int(11) DEFAULT NULL COMMENT '销量',
+  `lock_stock` int(11) DEFAULT '0' COMMENT '被锁定的库存',
+  `sp_data` varchar(500) DEFAULT NULL COMMENT '商品销售属性[{"key":"毛色","value":"黑色"},{"key":"年龄","value":"1岁"}]',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='sku(库存保有单位)的库存';
+
+-- ----------------------------
+-- Table structure for ums_admin
+-- ----------------------------
+DROP TABLE IF EXISTS `ums_admin`;
+CREATE TABLE `ums_admin` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(64) DEFAULT NULL,
+  `password` varchar(64) DEFAULT NULL,
+  `icon` varchar(500) DEFAULT NULL COMMENT '头像',
+  `email` varchar(100) DEFAULT NULL COMMENT '邮箱',
+  `nick_name` varchar(200) DEFAULT NULL COMMENT '昵称',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `status` int(1) DEFAULT '1' COMMENT '帐号启用状态：0->禁用；1->启用',
+  `token_identifier` varchar(64) DEFAULT NULL COMMENT 'jwt负载中存入此字段,每次比对,修改使token失效',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='后台用户表';
+
+-- ----------------------------
+-- Table structure for ums_admin_login_log
+-- ----------------------------
+DROP TABLE IF EXISTS `ums_admin_login_log`;
+CREATE TABLE `ums_admin_login_log` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `admin_id` bigint(20) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `ip` varchar(64) DEFAULT NULL,
+  `address` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='后台用户登录日志表';
+
+-- ----------------------------
+-- Table structure for ums_admin_role_relation
+-- ----------------------------
+DROP TABLE IF EXISTS `ums_admin_role_relation`;
+CREATE TABLE `ums_admin_role_relation` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `admin_id` bigint(20) DEFAULT NULL,
+  `role_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='后台用户和角色关系表';
+
+-- ----------------------------
+-- Table structure for ums_member
+-- ----------------------------
+DROP TABLE IF EXISTS `ums_member`;
+CREATE TABLE `ums_member` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(64) DEFAULT NULL COMMENT '用户名',
+  `password` varchar(64) DEFAULT NULL COMMENT '密码',
+  `nickname` varchar(64) DEFAULT NULL COMMENT '昵称',
+  `phone` varchar(64) DEFAULT NULL COMMENT '手机号码',
+  `status` int(1) DEFAULT NULL COMMENT '帐号启用状态:0->禁用；1->启用',
+  `create_time` datetime DEFAULT NULL COMMENT '注册时间',
+  `icon` varchar(500) DEFAULT NULL COMMENT '头像',
+  `gender` int(1) DEFAULT NULL COMMENT '性别：0->未知；1->男；2->女',
+  `birthday` date DEFAULT NULL COMMENT '生日',
+  `city` varchar(64) DEFAULT NULL COMMENT '所在城市',
+  `job` varchar(100) DEFAULT NULL COMMENT '职业',
+  `token_identifier` varchar(64) DEFAULT NULL COMMENT 'jwt负载中存入此字段,每次比对,修改使token失效',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE KEY `idx_username` (`username`) USING BTREE,
+  UNIQUE KEY `idx_phone` (`phone`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='会员表';
+
+-- ----------------------------
+-- Table structure for ums_member_login_log
+-- ----------------------------
+DROP TABLE IF EXISTS `ums_member_login_log`;
+CREATE TABLE `ums_member_login_log` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `member_id` bigint(20) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `ip` varchar(64) DEFAULT NULL,
+  `address` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='会员登录记录';
+
+-- ----------------------------
+-- Table structure for ums_member_receive_address
+-- ----------------------------
+DROP TABLE IF EXISTS `ums_member_receive_address`;
+CREATE TABLE `ums_member_receive_address` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `member_id` bigint(20) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL COMMENT '收货人名称',
+  `phone_number` varchar(64) DEFAULT NULL,
+  `default_status` int(1) DEFAULT NULL COMMENT '是否为默认收货地址',
+  `post_code` varchar(100) DEFAULT NULL COMMENT '邮政编码',
+  `province` varchar(100) DEFAULT NULL COMMENT '省份/直辖市',
+  `city` varchar(100) DEFAULT NULL COMMENT '城市',
+  `region` varchar(100) DEFAULT NULL COMMENT '区',
+  `detail_address` varchar(128) DEFAULT NULL COMMENT '详细地址(街道)',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='会员收货地址表';
+
+-- ----------------------------
+-- Table structure for ums_menu
+-- ----------------------------
+DROP TABLE IF EXISTS `ums_menu`;
+CREATE TABLE `ums_menu` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `parent_id` bigint(20) DEFAULT NULL COMMENT '父级ID',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `title` varchar(100) DEFAULT NULL COMMENT '菜单名称',
+  `level` int(4) DEFAULT NULL COMMENT '菜单级数,从0开始',
+  `sort` int(4) DEFAULT NULL COMMENT '菜单排序',
+  `name` varchar(100) DEFAULT NULL COMMENT '前端url名称',
+  `icon` varchar(200) DEFAULT NULL COMMENT '前端图标',
+  `hidden` int(1) DEFAULT NULL COMMENT '前端隐藏',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='后台菜单表';
+
+-- ----------------------------
+-- Table structure for ums_resource
+-- ----------------------------
+DROP TABLE IF EXISTS `ums_resource`;
+CREATE TABLE `ums_resource` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `name` varchar(200) DEFAULT NULL COMMENT '资源名称',
+  `url` varchar(200) DEFAULT NULL COMMENT '资源URL',
+  `description` varchar(500) DEFAULT NULL COMMENT '描述',
+  `category_id` bigint(20) DEFAULT NULL COMMENT '资源分类ID',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='后台资源表';
+
+-- ----------------------------
+-- Table structure for ums_resource_category
+-- ----------------------------
+DROP TABLE IF EXISTS `ums_resource_category`;
+CREATE TABLE `ums_resource_category` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `name` varchar(200) DEFAULT NULL COMMENT '分类名称',
+  `sort` int(4) DEFAULT NULL COMMENT '排序',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='资源分类表';
+
+-- ----------------------------
+-- Table structure for ums_role
+-- ----------------------------
+DROP TABLE IF EXISTS `ums_role`;
+CREATE TABLE `ums_role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL COMMENT '名称',
+  `description` varchar(500) DEFAULT NULL COMMENT '描述',
+  `admin_count` int(11) DEFAULT NULL COMMENT '后台用户数量',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `status` int(1) DEFAULT '1' COMMENT '启用状态：0->禁用；1->启用',
+  `sort` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='后台用户角色表';
+
+-- ----------------------------
+-- Table structure for ums_role_menu_relation
+-- ----------------------------
+DROP TABLE IF EXISTS `ums_role_menu_relation`;
+CREATE TABLE `ums_role_menu_relation` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `role_id` bigint(20) DEFAULT NULL COMMENT '角色ID',
+  `menu_id` bigint(20) DEFAULT NULL COMMENT '菜单ID',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='后台角色菜单关系表';
+
+-- ----------------------------
+-- Table structure for ums_role_resource_relation
+-- ----------------------------
+DROP TABLE IF EXISTS `ums_role_resource_relation`;
+CREATE TABLE `ums_role_resource_relation` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `role_id` bigint(20) DEFAULT NULL COMMENT '角色ID',
+  `resource_id` bigint(20) DEFAULT NULL COMMENT '资源ID',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC COMMENT='后台角色资源关系表';
+
+SET FOREIGN_KEY_CHECKS = 1;
