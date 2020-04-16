04491475768fca1c75d6d3f1f8d6c233afc86012
zack <cai.guanyu@qq.com>
2020-04-16 23:18:08 +0800

    修复import错误

--- backend/ouostore-service/src/main/java/org/csu/ouostore/service/impl/OmsCartItemServiceImpl.java
+++ backend/ouostore-service/src/main/java/org/csu/ouostore/service/impl/OmsCartItemServiceImpl.java
@@ -1,20 +1,18 @@
 package org.csu.ouostore.service.impl;
 
-import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.ObjectUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import org.csu.ouostore.common.exception.ApiException;
-import org.csu.ouostore.model.entity.*;
-import org.csu.ouostore.mapper.OmsCartItemMapper;
-import org.csu.ouostore.model.vo.OmsCartDetailVo;
-import org.csu.ouostore.model.vo.OmsOrderDetailVo;
-import org.csu.ouostore.service.OmsCartItemService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.csu.ouostore.common.exception.ApiException;
+import org.csu.ouostore.mapper.OmsCartItemMapper;
+import org.csu.ouostore.model.entity.OmsCartItem;
+import org.csu.ouostore.model.entity.UmsMember;
+import org.csu.ouostore.model.vo.OmsCartDetailVo;
+import org.csu.ouostore.service.OmsCartItemService;
 import org.csu.ouostore.service.UmsMemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.jws.Oneway;
 import java.util.List;
 
 /**
