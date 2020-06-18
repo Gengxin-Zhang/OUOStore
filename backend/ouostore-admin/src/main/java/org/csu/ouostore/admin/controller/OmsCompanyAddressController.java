e533d2693ac962dff24dd6cf56aaa8d5cd1fc4f4
zack <cai.guanyu@qq.com>
2020-06-18 16:28:50 +0800

    完成company_address的api

---
+++ backend/ouostore-admin/src/main/java/org/csu/ouostore/admin/controller/OmsCompanyAddressController.java
@@ -0,0 +1,41 @@
+package org.csu.ouostore.admin.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.csu.ouostore.common.api.CommonResult;
+import org.csu.ouostore.model.entity.OmsCompanyAddress;
+import org.csu.ouostore.service.OmsCompanyAddressService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 发货地址地址管理Controller
+ * @author zack
+ */
+@RestController
+@Api(tags = "发货地址管理")
+@RequestMapping("/api/v1/company_addresses")
+public class OmsCompanyAddressController {
+
+    @Autowired
+    private OmsCompanyAddressService companyAddressService;
+
+    @ApiOperation("获取所有发货地址")
+    @GetMapping(value = "")
+    public CommonResult<List<OmsCompanyAddress>> list() {
+        List<OmsCompanyAddress> companyAddressList = companyAddressService.list();
+        return CommonResult.OK(companyAddressList);
+    }
+
+    @ApiOperation("添加发货地址")
+    @PostMapping(value = "")
+    public CommonResult<String> create(OmsCompanyAddress address) {
+        boolean success = companyAddressService.save(address);
+        return success ? CommonResult.OK("创建成功") : CommonResult.failed("创建失败,未知错误");
+    }
+}
--- backend/ouostore-model/src/main/java/org/csu/ouostore/model/entity/OmsCompanyAddress.java
+++ backend/ouostore-model/src/main/java/org/csu/ouostore/model/entity/OmsCompanyAddress.java
@@ -2,12 +2,13 @@ package org.csu.ouostore.model.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
-import java.io.Serializable;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
+
+import java.io.Serializable;
 
 /**
  * <p>
@@ -26,33 +27,34 @@ public class OmsCompanyAddress implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @TableId(value = "id", type = IdType.AUTO)
+    @ApiModelProperty(hidden = true)
     private Long id;
 
-    @ApiModelProperty(value = "地址名称")
+    @ApiModelProperty(value = "地址名称", example = "深圳发货点")
     private String addressName;
 
-    @ApiModelProperty(value = "默认发货地址：0->否；1->是")
+    @ApiModelProperty(value = "默认发货地址：0->否；1->是", example = "1")
     private Integer sendStatus;
 
-    @ApiModelProperty(value = "是否默认收货地址：0->否；1->是")
+    @ApiModelProperty(value = "是否默认收货地址：0->否；1->是", example = "1")
     private Integer receiveStatus;
 
-    @ApiModelProperty(value = "收发货人姓名")
+    @ApiModelProperty(value = "收发货人姓名", example = "zack")
     private String name;
 
-    @ApiModelProperty(value = "收货人电话")
+    @ApiModelProperty(value = "收发货人电话", example = "17755555555")
     private String phone;
 
-    @ApiModelProperty(value = "省/直辖市")
+    @ApiModelProperty(value = "省/直辖市", example = "广东省")
     private String province;
 
-    @ApiModelProperty(value = "市")
+    @ApiModelProperty(value = "市", example = "深圳市")
     private String city;
 
-    @ApiModelProperty(value = "区")
+    @ApiModelProperty(value = "区", example = "福田区")
     private String region;
 
-    @ApiModelProperty(value = "详细地址")
+    @ApiModelProperty(value = "详细地址", example = "梅林路173号")
     private String detailAddress;
 
 
