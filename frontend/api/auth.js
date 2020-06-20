d783639c9931813ac8abe5e687d723dba2e939c2
Gengxin-Zhang <zhgx3ng@outlook.com>
2020-06-21 01:47:46 +0800

    更换前端代码目录

--- frontend/api/auth.js
+++
@@ -1,9 +0,0 @@
-export default ($axios) => ({
-    signin(credentials) {
-      return $axios.post("/admin/oauth/access_token", credentials)
-    },
-    validToken(token) {
-      return $axios.post("/auth/valid_token", {token})
-    }
-  });
-  
--- frontend/plugins/api.js
+++
@@ -1,9 +0,0 @@
-import Auth from "@/api/auth"
-
-export default (context, inject) => {
-  // Initialize API repositories
-  const repositories = {
-    auth: Auth(context.$axios),
-  };
-  inject("api", repositories);
-};
